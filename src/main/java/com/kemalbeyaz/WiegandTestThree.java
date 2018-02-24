47e64f050d413cfe218effeca3aa228136913daf
Kemal B <kemal@chcdesign.com>
2018-02-24 14:44:58 +0300

    Listener Test... 2

--- src/main/java/com/kemalbeyaz/WiegandTestThree.java
+++ src/main/java/com/kemalbeyaz/WiegandTestThree.java
@@ -4,7 +4,17 @@ import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
-public class WiegandTestThree {
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WiegandTestThree extends TimerTask{
+
+    public static char[] s = new char[26];
+    static int bits = 0;
+
+    public WiegandTestThree() { }
+
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("pi4j.linking", "dynamic");
 
@@ -16,6 +26,10 @@ public class WiegandTestThree {
         final GpioPinDigitalInput pin0 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, PinPullResistance.PULL_UP);
         final GpioPinDigitalInput pin1 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, PinPullResistance.PULL_UP);
 
+        // Timer test...
+        TimerTask timerTask = new WiegandTestThree();
+        Timer timer = new Timer(true);
+
         System.out.println("PINs ready");
 
         pin0.addListener(new GpioPinListenerDigital() {
@@ -23,6 +37,8 @@ public class WiegandTestThree {
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
                 // display pin state on console
                 System.out.println("0 --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+                timer.scheduleAtFixedRate(timerTask, 0, 5*1000);
+                System.out.println("Bitti " + new Date());
             }
 
         });
@@ -36,10 +52,20 @@ public class WiegandTestThree {
 
         });
 
+
+
         // keep program running until user aborts (CTRL-C)
         while(true) {
             Thread.sleep(500);
         }
 
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("Hey! Çalıştım " +new Date());
+
+        }
     }
 }
